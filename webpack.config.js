12fd7770be5aed8657d63f0a797fbd9196503569
KAUSHIK SARKAR <kaushikisonline@gmail.com>
2018-12-25 21:26:54 +0530

    Add files via upload
    Install babel-core; babel-preset-env, babel-loader and babel-polyfill through command prompt using the following commands:
    npm install @babel/core --save-dev
    npm install --save-dev @babel/preset-env
    npm install babel-loader --save-dev
    npm install @babel/polyfill --save
    
    Check installation status from package.json. On successfull installation of babel, names of all the packages will appear in the file.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['@babel/polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        host: '127.0.0.1',
+        port: 8080,
+        https: false,
+        disableHostCheck: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
